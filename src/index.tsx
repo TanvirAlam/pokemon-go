24dff3f0aef9331cd5300d8b2088ed7ae0473ecc
TAlam <tanvir.alam.shawn@gmail.com>
2022-10-04 11:26:57 +0200

    Adding Themes and devices for mobile and consistent styling

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from './theme';
 import GlobalStyles from './globalStyles';
 
 const root = ReactDOM.createRoot(
@@ -12,7 +13,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter basename={process.env.PUBLIC_URL}>
         <GlobalStyles />
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
           <App />
         </ChakraProvider>
     </BrowserRouter>
