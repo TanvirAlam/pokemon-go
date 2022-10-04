8294b554bcad46b0df364c07b30c0ec92bef6249
TAlam <tanvir.alam.shawn@gmail.com>
2022-10-04 11:22:11 +0200

    Adding global styles and incorporating chakra provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider } from "@chakra-ui/react";
+import GlobalStyles from './globalStyles';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <GlobalStyles />
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
